8fec06c9d14fe0bbe564fc7241142a3d9958300f
Celyne <dianakydd2@gmail.com>
2024-03-18 20:34:36 +0300

    Added text3.py
    evoked readline()

---
+++ text3.py
@@ -0,0 +1,18 @@
+from os import strerror
+
+try:
+    character_counter = line_counter = 0
+    stream = open('text.txt', 'rt')
+    line = stream.readline()
+    while line != '':
+        line_counter += 1
+        for char in line:
+            print(char, end='')
+            character_counter += 1
+        line = stream.readline()
+    stream.close()
+    print("\n\nCharacters in file:", character_counter)
+    print("Lines in file:     ", line_counter)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
+    
